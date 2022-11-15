40ac5b4c929d172a871aa028f90fc06b18ad98f8
Liam Junkermann <dev@ljsoft.xyz>
2022-11-15 20:13:57 +0000

    removing unneeded network proto

--- update_protos.sh
+++ update_protos.sh
@@ -3,7 +3,7 @@ DIR=dota
 rm -rf $DIR
 svn export https://github.com/SteamDatabase/GameTracking-Dota2.git/trunk/Protobufs $DIR
 # removing files from update protobufs
-rm -rf $DIR/gametoolevents.proto $DIR/dota_messages_mlbot.proto $DIR/dota_gcmessages_common_bot_script.proto $DIR/steammessages_base.proto $DIR/steammessages_clientserver_login.proto $DIR/*steamworks*.proto $DIR/tensorflow
+rm -rf $DIR/gametoolevents.proto $DIR/dota_messages_mlbot.proto $DIR/dota_gcmessages_common_bot_script.proto $DIR/steammessages_base.proto $DIR/steammessages_clientserver_login.proto $DIR/*steamworks*.proto $DIR/tensorflow $DIR/netowkr_connection.proto
 for f in $DIR/*.proto; do mv "$f" "$(echo "$f" | sed s/.proto$/_m.proto/)"; done
 sed -i '/import "google\/protobuf\//! s/.proto";/_m.proto";/g' $DIR/*.proto
 sed -i 's/^\(\s*\)\(optional\|repeated\|required\|extend\)\s*\./\1\2 /' $DIR/*.proto
